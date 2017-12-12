eea7068294964ee41ead94c0203845fa2907de3e
emilio barrera <barrera_emilio@yahoo.es>
2017-12-12 23:02:04 +0000

    all properties ready

--- ForeignExchange1/ForeignExchange1/ViewModel/MainViewModel.cs
+++ ForeignExchange1/ForeignExchange1/ViewModel/MainViewModel.cs
@@ -1,12 +1,83 @@
 ﻿  
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
 namespace ForeignExchange1.ViewModel
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+    using System.Collections.ObjectModel;
+    using ForeignExchange1.Models;
 
     public class MainViewModel
     {
+        #region Properties
+
+        public string Amount
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+
+        public Rate SourceRate
+        {
+            get;
+            set;
+        }
+
+        public Rate TargetRate
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public object IsEnable
+        {
+            get;
+            set; 
+        }
+
+        public string Result
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+
+        #region Commands
+
+        public ICommand ConvertCommand
+        {
+            get { return new RelayCommand(convert); }
+        }
+
+        
+
+        #endregion
+
+        #region Methods
+
+        private void convert()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
 
     }
 }
