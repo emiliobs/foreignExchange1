6a1997abd5a3f536c72c2ee166014d0f1b3c907a
emilio barrera <barrera_emilio@yahoo.es>
2018-01-18 01:00:41 +0000

    implementation of the interface of the different languaje in developent

---
+++ ForeignExchange1/ForeignExchange1/Helpers/Languages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ForeignExchange1.Interfaces;
+using ForeignExchange1.Resources;
+using Xamarin.Forms;
+
+namespace ForeignExchange1.Helpars
+{
+    public class Languages
+    {
+
+        static Languages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Accept
+        {
+            get { return Resource.Accept; }
+
+        }
+
+        public static string AmountValidation
+        {
+            get { return Resource.AmountValidation; }
+        }
+        public static string Error
+        {
+            get { return Resource.Error; }
+        }
+        public static string Title
+        {
+            get { return Resource.Title; }
+        }
+
+    }
+}
---
+++ ForeignExchange1/ForeignExchange1/Interfaces/ILocalize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ForeignExchange1.Interfaces
+{
+    public interface ILocalize
+    {
+
+        //dos métodos
+        CultureInfo GetCurrentCultureInfo();
+        void SetLocale(CultureInfo ci);
+    }
+}
